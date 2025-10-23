73411194fda45869825d245d054e0c788f692629
Tamir Udelgombo <udelgombotamira@gmail.com>
2025-10-23 20:10:19 +0800

    Prepare for Vercel deployment

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true, // Only use this if you're having typescript issues during deployment
+  }
+};
+
+module.exports = nextConfig;
